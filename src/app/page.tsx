175ed4b61e2caf48a9e479a47ef1a17c9810c260
Emmanuel Uwem <emmanueluwem560@gmail.com>
2025-09-12 17:22:53 +0100

    feat(ui): copy role selection page from onboarding page to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,67 @@
-"use client";
+import React from "react";
+import UserTypeCard from "./onboarding/signup/components/UserTypeCardProps";
+import Image from "next/image";
+import Link from "next/link";
 
-import React, { useState } from "react";
-import BioDataForm from "./onboarding/volunteer/components/screens/bioDataForm";
-import LocationForm from "./onboarding/volunteer/components/screens/locationForm";
-import { VolunteerFormData } from "@/types";
-import Layout from "./onboarding/volunteer/components/layout";
-import InterestAndCauseForm from "./onboarding/volunteer/components/screens/InterestAndCauseForm";
-import SkillsAndStrengthForm from "./onboarding/volunteer/components/screens/SkillsAndStrengthForm";
+//usertypecard details
+const userType = [
+  {
+    text: "Volunteer",
+    img: "/Man.svg",
+    alt: "Volunteer",
+    link: "/onboarding/signup/volunteer",
+  },
+  {
+    text: "Organisation",
+    img: "/People.svg",
+    alt: "Organisation",
+    link: "/onboarding/signup/org",
+  },
+];
 
-const VolunteerOnboarding: React.FC = () => {
-  const [step, setStep] = useState<number>(0);
-  const [formData, setFormData] = useState<VolunteerFormData>({
-    firstName: "",
-    lastName: "",
-    otherNames: "",
-    sex: "",
-    dob: "",
-    country: "",
-    state: "",
-    city: "",
-    zip: "",
-    address: "",
-  });
+//for the styling of the volunteer and org text
+const styleItem = "sm:text-[32px]  font-[400] text-[#ODODOD]]";
 
-  const screens = [
-    <BioDataForm key="bio" formData={formData} setFormData={setFormData} />,
-    <LocationForm
-      key="location"
-      formData={formData}
-      setFormData={setFormData}
-      />,
-      <InterestAndCauseForm key={"interest"} />,
-    <SkillsAndStrengthForm key={"skills"} />,
-
-  ];
-
+const page = () => {
   return (
-    <Layout
-      step={step}
-      totalSteps={screens.length}
-      onNext={() => setStep((prev) => Math.min(prev + 1, screens.length - 1))}
-      onBack={() => setStep((prev) => Math.max(prev - 1, 0))}
-    >
-      {screens[step]}
-    </Layout>
+    <>
+      <h1 className="md:text-6xl  text-4xl font-[700] pt-[130px] sm:pt-[100px] text-center">
+        Welcome
+      </h1>
+      <p className="pt-[6px] sm:text-[20px] text-[13px] text-center">
+        Please select the category that aligns with your goals
+      </p>
+      <div className="flex gap-[22px] ">
+        {userType.map((user, index) => (
+          <Link href={user.link} key={index}>
+            <UserTypeCard key={index}>
+              <Image
+                src={user.img}
+                width={70}
+                height={70}
+                className="md:w-[120px] md:h-[120px] sm:w-[100px] sm:h-[100px]"
+                alt={user.alt}
+              />
+              <h2 className={styleItem}>{user.text}</h2>
+            </UserTypeCard>
+          </Link>
+        ))}
+      </div>
+      <p className="pt-[30px] text-[16px]">
+        Already have an account?{" "}
+        <span className="text-primary font-[700]">
+          <Link href="/login">Sign In</Link>
+        </span>
+      </p>
+      <Image
+        src="/user.svg"
+        alt="illustration-svg"
+        width={446}
+        height={223}
+        className="fixed bottom-0 z-[-1] sm:block hidden"
+      />
+    </>
   );
 };
 
-export default VolunteerOnboarding;
+export default page;
